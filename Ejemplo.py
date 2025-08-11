3cf36690f88cadf43fa4e959c8f296c4188a8253
AlanHerr <ayherrerar@academia.usbbog.edu.co>
2025-08-11 14:01:04 -0500

    doc: new comment

--- Ejemplo.py
+++ Ejemplo.py
@@ -3,6 +3,8 @@ print ("Hola Mundo")
 def hola_mundo():
     print("Hola desde la función")
 
+#Esto es cine y es pura dopamina.
+
 if __name__ == "__main__":
     hola_mundo()
     print("Este es un ejemplo de código en Python.")
